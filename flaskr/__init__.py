3cd3dace9069dbf45d3671685ca557a90a405d67
Jamie VanDernoot <james.vandernoot@slalom.com>
2020-09-07 20:26:53 -0500

    Adding in base pages for login and registration.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -11,7 +12,7 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
@@ -27,5 +28,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
